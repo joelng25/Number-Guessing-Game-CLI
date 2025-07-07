0d145402e7eb9909af04fe338bf9116ce80682eb
joelng <146856838+joelng25@users.noreply.github.com>
2025-07-07 10:43:14 +0200

    Fixed errors
    Fixed bugs handling input

--- main.cc
+++ main.cc
@@ -9,9 +9,6 @@ int answer;
 int guess, guess_count, attempts = 1;
 
 int main() {
-  srand(time(0));
-  answer = rand() % 100 + 1;
-
   cout << "Welcome to the Number Guessing Game!" << endl
        << "I'm thinking of a number between 1 and 100." << endl
        << "You have 5 chances to guess the correct number." << endl
@@ -24,7 +21,15 @@ int main() {
        << endl;
 
   cout << "Enter your choice: ";
-  cin >> diff;
+
+  while (!(cin >> diff) || diff < 1 || diff > 3) {
+    if (cin.fail()) {
+      cin.clear();
+      cout << "Invalid input! Please enter a number between 1 and 3: ";
+    } else {
+      cout << "Invalid choice! Please enter 1, 2, or 3: ";
+    }
+  }
 
   switch (diff) {
     case 1:
@@ -41,24 +46,45 @@ int main() {
       cout << "Great! You have selected the Hard difficulty level." << endl;
       guess_count = 3;
       break;
+
+    default:
+      cout << "Invalid choice. Defaulting to Medium difficulty." << endl;
+      guess_count = 5;
+      break;
   }
 
   cout << "Let's start the game!" << endl;
 
-  while (guess_count > 0) {
-    cout << "Enter your guess: ";
-    cin >> guess;
+  string play_again = "Y";
+  while (play_again == "Y" or play_again == "y") {
+    srand(time(0));
+    answer = rand() % 100 + 1;
+    attempts = 1;
+    while (guess_count != attempts - 1) {
+      cout << "Enter your guess: ";
+      while (!(cin >> guess) || guess < 1 || guess > 100) {
+        if (cin.fail()) {
+          cin.clear();              // Clear the error flag
+          cin.ignore(10000, '\n');  // Remove the invalid input from buffer
+          cout << "Invalid input! Please enter a number between 1 and 100: ";
+        } else {
+          cout << "Number out of range! Please enter a number between 1 and 100: ";
+        }
+      }
 
-    if (guess == answer) {
-      cout << "Congratulations! You guessed the correct number in " << attempts << " attempts."
-           << endl;
-      return 1;
-    } else if (guess < answer) {
-      cout << "Incorrect! The number is greater than " << guess << "." << endl;
-    } else {
-      cout << "Incorrect! The number is less than " << guess << "." << endl;
+      if (guess == answer) {
+        cout << "Congratulations! You guessed the correct number in " << attempts << " attempts."
+             << endl;
+        guess_count = 0;
+      } else if (guess < answer) {
+        cout << "Incorrect! The number is greater than " << guess << "." << endl;
+      } else {
+        cout << "Incorrect! The number is less than " << guess << "." << endl;
+      }
+      ++attempts;
     }
-    --guess_count;
-    ++attempts;
+    cout << "Want to play again? (Write Y or N)" << endl;
+    cin >> play_again;
   }
+  return 1;
 }
